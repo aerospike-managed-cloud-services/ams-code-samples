408d1dc09c8e35b8c9a38b2668ade03f5c800d81
John Erick Ignacio <john.ignacio@nextreason.com>
2021-10-18 13:02:18 -0700

    added csharp sample code

--- csharp/sample.cs
+++ csharp/sample.cs
@@ -1,7 +1,28 @@
+using System;
+using System.IO;
+using Aerospike.Client;
 
-// Creating Connection
-ClientPolicy policy = new ClientPolicy();
-AerospikeClient client = new AerospikeClient(policy, new Host("localhost0", 3000), new Host("localhost1", 3001), new Host("localhost2", 3002));
+namespace Test
+{
+    public class ConnectionTest
+    {
+        public static void RunTest()
+        {
+            // Configure seed nodes
+            Host[] hosts = new Host[3];
+            hosts[0] = new Host("localhost0", 3000);
+            hosts[1] = new Host("localhost1", 3001);
+            hosts[2] = new Host("localhost2", 3002);
 
-// Closing Connection
-client.Close();
+            // Creating policy and configure username and password
+            ClientPolicy policy = new ClientPolicy();
+            policy.user = "aerospike_user";
+            policy.password = "aerospike_pass";
+            policy.failIfNotConnected = true;
+
+            AerospikeClient client = new AerospikeClient(policy, hosts);
+
+            client.Close();
+        }
+    }
+}
